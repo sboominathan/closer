09f3622cc2ff15a9850dbed3b8512a83ba00b6ea
Sooraj Boominathan <sooraj.boomi@gmail.com>
2016-01-10 17:16:36 -0500

    login page changed

---
+++ closer/db-setup.js
@@ -0,0 +1,38 @@
+var mongo = require('mongodb').MongoClient;
+
+var dbConnectionUrl = 'mongodb://localhost:27017/closerdb';
+
+var collections = {};
+
+mongo.connect(dbConnectionUrl, function (err, db) {
+  if (err) {
+    console.log('Can not connect to MongoDB. Did you run it?');
+    console.log(err.message);
+    return;
+  }
+
+  collections.users = db.collection('users');
+});
+
+
+module.exports = collections;
+
+
+
+/*var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/userdb', function(err) {
+    if(err) {
+        console.log('connection error', err);
+    } else {
+        console.log('connection successful');
+    }
+});
+
+var UserSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+
+module.exports = mongoose.model('User', UserSchema);*/
+
--- closer/routes/index.js
+++ closer/routes/index.js
@@ -1,9 +1,61 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../db-setup.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Closer' });
 });
+
+router.post('/login', function(req, res, next) {
+  
+  var username = req.body.username;
+  var password = req.body.password;
+  
+  db.users.find({user: username, password: password}).toArray(function (err, peeps) {
+    if (peeps.length > 0) {
+      // the user needs to be updated
+      res.send("Welcome "+ username);
+      }
+     else {
+      	
+        res.redirect('/');
+
+      }
+    
+     });
+
+});
+
+router.post("/signup", function(req, res, next){
+
+	var username = req.body.username;
+	var password = req.body.password;
+	var passwordcheck = req.body.passwordcheck;
+
+
+	
+  db.users.find({user: username}).toArray(function (err, peeps) {
+    
+  	//username already in use
+    if (peeps.length > 0) { 
+      // the user needs to be updated
+      res.redirect("/"); //render index page with notification that the username has already been taken
+      }
+     else if (password == passwordcheck) { 
+
+      	db.users.insert({user: username, password: password}, function(err){
+      		res.send('Thanks for signing up ' + username );
+      	});
+        
+      }
+    
+     });
+
+	
+
+});
+
+
 
 module.exports = router;
