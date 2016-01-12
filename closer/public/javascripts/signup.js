8712372b1e81cb2a994da6800005754992515c63
Sooraj Boominathan <sooraj.boomi@gmail.com>
2016-01-11 20:17:20 -0500

    sign up form complete

--- closer/public/javascripts/signup.js
+++ closer/public/javascripts/signup.js
@@ -1,17 +1,24 @@
 $(function(){
+    count = 1
     
     $(document).on('focus', 'div.form-group-options div.input-group-option:last-child input', function(){
         
         var sInputGroupHtml = $(this).parent().html();
         var sInputGroupClasses = $(this).parent().attr('class');
         $(this).parent().parent().append('<div class="'+sInputGroupClasses+'">'+sInputGroupHtml+'</div>');
+        count+=1
         
     });
     
     $(document).on('click', 'div.form-group-options .input-group-addon-remove', function(){
         
+        if (count>1){
         $(this).parent().remove();
+        count-=1;}
+
         
     });
+
+
     
 });
--- closer/routes/index.js
+++ closer/routes/index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var db = require("../db-setup.js");
+var currUser = null;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Closer' });
 });
+
+
+//PRESSING LOGIN ON FRONT PAGE
 
 router.post('/login', function(req, res, next) {
   
@@ -14,23 +18,40 @@ router.post('/login', function(req, res, next) {
   
   db.users.find({user: username, password: password}).toArray(function (err, peeps) {
     if (peeps.length > 0) {
-      // the user needs to be updated
-      res.send("Welcome "+ username);
+      
+      
+      //set currUser to user based on search in user array
+
+      db.users.find({user:username}).toArray(function(err,peeps){
+
+       		currUser = peeps[0];
+       		if (currUser.filledOut){
+       			res.render("matches", {title: "Closer"});
+       		}
+       		else{
+       			res.render("signup", {title: "Closer"});
+       		}
+      	});
+      
       }
      else {
       	
-        res.redirect('/');
+        res.send("Sorry, that's an incorrect username/password combination!");
 
       }
     
      });
 
 });
+////////////////////////////////
 
+
+//PRESSING SIGN UP ON FRONT PAGE
 router.post("/signup", function(req, res, next){
 
 	var username = req.body.username;
 	var password = req.body.password;
+	var email = req.body.email;
 	var passwordcheck = req.body.passwordcheck;
 
 
@@ -44,9 +65,17 @@ router.post("/signup", function(req, res, next){
       }
      else if (password == passwordcheck) { 
 
-      	db.users.insert({user: username, password: password}, function(err){
+      	db.users.insert({user: username, password: password, email: email, filledOut:false}, function(err){
+      		
       		res.render("signup", {title: "Closer"});
       	});
+       db.users.find({user:username}).toArray(function(err,peeps){
+
+       		currUser = peeps[0];
+
+       		
+      	})
+      	
         
       }
     
@@ -54,11 +83,71 @@ router.post("/signup", function(req, res, next){
 
 });
 
+////////////////////////////////
+
+router.post("/userinfo", function(req,res,next){
+
+	var first = req.body.firstname;
+	var last = req.body.lastname;
+	var college = req.body.college;
+	var year = req.body.year;
+	var gender = req.body.gender;
+	var courses = req.body.option;
+	var bio = req.body.bio;
+	
+
+	db.users.update(
+    { user: currUser.user },
+    {
+      $set: {
+        first: first,
+        last: last,
+        college: college,
+        year: year,
+        gender: gender,
+        courses: courses,
+        bio:bio,
+        filledOut: true
+      }
+      
+    }
+		)		
+	res.render('signup', { title: 'Closer' });
+
+})
+
+//SHORTCUT FOR SIGNUP (TESTING)
+
 router.get('/signup', function(req, res, next) {
+	//search for username entered and set current user to this value
   res.render('signup', { title: 'Closer' });
-  console.log("hi");
+   
 });
 
+//ABOUT PAGE
+router.get("/about", function(req,res,next){
+
+	
+	res.render("about", {title: "Closer"});
+
+});
+
+//CONTACT PAGE
+
+router.get("/contact", function(req,res,next){
+
+	res.render("contact", {title: "Closer"});
+
+});
+
+//REROUTES TO FRONT PAGE, RESETS CURRENT USER TO NULL
+
+router.get("/logout", function(req,res,next){
+	console.log("hihi")
+	var currUser = null;
+	res.redirect("/");
+	
+});
 
 
 module.exports = router;
